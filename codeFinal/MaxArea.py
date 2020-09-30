0048467e111c7e0f52b23b17003cf646cf95bcdc
DorskFR <42279120+DorskFR@users.noreply.github.com>
2020-09-30 17:46:49 +0900

    cleaning

--- codeFinal/MaxArea.py
+++ codeFinal/MaxArea.py
@@ -11,22 +11,11 @@ def contour_max_area(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
-        # find the biggest contour (c) by the area
+        # find the biggest contour by the area
         c = max(contours, key = cv2.contourArea)
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        print(box)
-        print(type(box))
-        #img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        #cv2.drawContours(img, [box], 0, (0, 255, 255), 3)
-
-        # draw the biggest rectangle not vertical
-        # for cnt in contours:
-        #     rect = cv2.minAreaRect(cnt)
-        #     box = cv2.boxPoints(rect)
-        #     box = np.int0(box)
-        #     cv2.drawContours(img, [box], 0, (0, 255, 255), 25)
     else:
         box = np.zeros((4, 2), dtype=int)
 
